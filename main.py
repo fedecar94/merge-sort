908dcf49f8a8108adfeb44283864679883d34245
Federico Cardozo <fedecar94@outlook.com>
2021-06-16 21:41:24 -0400

    less memory usage

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
-def merge_sort(array: list) -> list:
-    if len(array) == 1:
-        return array
+def merge_sort(array: list):
+    size = len(array)
+    if size == 1:
+        return
 
-    new_array = []
+    mid = size // 2
 
-    mid = len(array) // 2
+    left = array[:mid]
+    merge_sort(left)
 
-    left = merge_sort(array[:mid])
+    right = array[mid:]
+    merge_sort(right)
 
-    right = merge_sort(array[mid:])
+    index = 0
 
     while len(left) and len(right):
         if left[0] < right[0]:
-            new_array.append(left.pop(0))
+            array[index] = left.pop(0)
+            index += 1
         else:
-            new_array.append(right.pop(0))
+            array[index] = right.pop(0)
+            index += 1
 
     if len(left):
-        new_array.extend(left)
+        array[index: index + len(left)] = left
+        index += len(left)
 
     if len(right):
-        new_array.extend(right)
-
-    return new_array
+        array[index:] = right
 
 
 if __name__ == '__main__':
     import random
-    randomlist = []
-    for i in range(0, 10):
-        n = random.randint(1, 30)
-        randomlist.append(n)
-    print(merge_sort(randomlist))
+
+    randomlist = [random.randint(1, 30) for x in range(0, 10)]
+
+    print(randomlist)
+    merge_sort(randomlist)
+    print(randomlist)
